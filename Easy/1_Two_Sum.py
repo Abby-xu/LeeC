f4bb2b46cb3053421bfc094091cc5147162916e0
Abby-xu <abby.xu915@gmail.com>
2021-08-02 04:32:58 +0000

    8/1/2021

--- Easy/1_Two_Sum.py
+++ Easy/1_Two_Sum.py
@@ -1,6 +1,15 @@
+from typing import List
+
 class Solution:
     def twoSum(self, nums: List[int], target: int) -> List[int]:
         for i in range(len(nums)):
-            for j in range(i, len(nums)):
-                if i != j and nums[i] + nums[j] == target:
+            for j in range(i+1, len(nums)):
+                if nums[i] + nums[j] == target:
                     return [i,j]
+
+### test ###
+nums = [1,2,3,4,5,6,7,8,9,10]
+target = 8
+
+s = Solution()
+print(s.twoSum(nums,target))
