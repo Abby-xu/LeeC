43cd19a1872fe3a8a2f842a28d520677fb6ea197
Abby-xu <abby.xu915@gmail.com>
2021-08-04 05:58:13 +0000

    4/8/2021

---
+++ Hard/1168_Optimize_Water_Distribution_in_a_Village.py
@@ -0,0 +1,33 @@
+class Solution:
+    def minCostToSupplyWater(self, n: int, wells: List[int], pipes: List[List[int]]) -> int:
+        parents = [i for i in range(n+1)]
+        
+        # add pipes to each house from the well, node zero.
+        for i, cost in enumerate(wells):
+            pipes.append([0, i+1, cost])
+        
+        # run kruskal algo to build minimum spanning tree.
+        # first, sort by cost
+        # start picking the lowest cost edges until we visited all nodes.
+        pipes.sort(key=lambda x:x[2])
+                
+        def find(u):
+            while u != parents[u]:
+                parents[u] = parents[parents[u]]
+                u = parents[u]
+            return u
+        
+        def union(u, v):
+            pu = find(u)
+            pv = find(v)
+            
+            if pu != pv:
+                parents[pu] = pv
+            return
+        
+        res = 0
+        for u, v, cost in pipes:
+            if find(u) != find(v):
+                union(u, v)
+                res += cost
+        return res
---
+++ Medium/90_Subest_II.py
@@ -0,0 +1,13 @@
+class Solution:
+    def subsetsWithDup(self, nums: List[int]) -> List[List[int]]:
+        nums.sort()
+        out = []
+        def dfs(curr, nums):                          
+            out.append(curr[:]) 
+            for i in range(len(nums)):
+                if i > 0 and nums[i-1] == nums[i]:     
+                    continue
+                dfs(curr+[nums[i]], nums[i+1:])      
+                
+        dfs([],nums)                             
+        return out
