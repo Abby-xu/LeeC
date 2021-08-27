1f2e00ae113df05af8648a62a1b50b3a2b49e90b
Abby Xu <39939031+Abby-xu@users.noreply.github.com>
2021-08-26 23:27:09 -0700

    8/26/2021

---
+++ Medium/633_Sum_of_Square_Numbers.py
@@ -0,0 +1,11 @@
+class Solution:
+    def judgeSquareSum(self, c: int) -> bool:
+        left = 0
+        right = int(sqrt(c))
+        while left <= right:
+            curr = left ** 2 + right ** 2
+            print(curr, c)
+            if curr == c: return True
+            if curr < c: left += 1
+            else: right -= 1
+        return False
