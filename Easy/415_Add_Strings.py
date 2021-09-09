3cafc7e7c0768137e75001bac63c661cf9c0f5f3
Abby Xu <39939031+Abby-xu@users.noreply.github.com>
2021-09-09 01:05:35 -0500

    9/9/2021

--- Easy/415_Add_Strings.py
+++ Easy/415_Add_Strings.py
@@ -1,3 +1,18 @@
 class Solution:
     def addStrings(self, num1: str, num2: str) -> str:
-        return str(int(num1)+int(num2))
+#         return str(int(num1)+int(num2))
+
+        i, j, carry = len(num1)-1, len(num2)-1, 0
+        out = []
+        while i >= 0 or j >= 0 or carry > 0:
+            n1, n2 = 0, 0
+            if i >= 0:
+                n1 = int(num1[i])
+                i -= 1
+            if j >= 0:
+                n2 = int(num2[j])
+                j -= 1
+            sums = n1 + n2 + carry
+            carry = 1 if sums >= 10 else 0
+            out.insert(0, str(sums%10))
+        return ''.join(out)
