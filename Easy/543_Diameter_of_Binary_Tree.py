2a4a230bc4b3943b269b64e3bf17c124ae162e33
Abby Xu <39939031+Abby-xu@users.noreply.github.com>
2021-10-10 21:02:57 -0500

    10/10/2021

---
+++ Easy/543_Diameter_of_Binary_Tree.py
@@ -0,0 +1,17 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def diameterOfBinaryTree(self, root: Optional[TreeNode]) -> int:
+        self.out = 0
+        def depth(root):
+            if not root:
+                return 0
+            left, right = depth(root.left), depth(root.right)
+            self.out = max(self.out, left+right)
+            return 1+max(left, right)
+        depth(root)
+        return self.out
