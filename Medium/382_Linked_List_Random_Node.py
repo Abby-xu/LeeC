271c482f9310eaaaa41a68c53c86672c9e019f35
Abby Xu <39939031+Abby-xu@users.noreply.github.com>
2022-01-06 21:24:59 -0600

    1/6/2022

---
+++ Medium/382_Linked_List_Random_Node.py
@@ -0,0 +1,18 @@
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class Solution:
+
+    def __init__(self, head: Optional[ListNode]):
+        self.head = head
+
+    def getRandom(self) -> int:
+        result, node, index = self.head, self.head.next, 1
+        while node:
+            if random.randint(0, index) is 0:
+                result = node
+            node = node.next
+            index += 1
+        return result.val
