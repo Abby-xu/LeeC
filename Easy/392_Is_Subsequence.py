abfc41241589aac93188dd2ca8b713d988dcf42c
Abby Xu <39939031+Abby-xu@users.noreply.github.com>
2022-03-01 18:26:04 -0600

    3/1/2022

---
+++ Easy/392_Is_Subsequence.py
@@ -0,0 +1,10 @@
+class Solution:
+    def isSubsequence(self, s: str, t: str) -> bool:
+        if not s: return True
+        pos = 0
+        for ch in t:
+            if ch == s[pos]:
+                pos += 1
+                if pos == len(s):
+                    return True
+        return False
